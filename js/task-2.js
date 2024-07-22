e6c2dc94b8cd57644db70dfcc3bc169526f8c29a
Ivan <159075250+1vengo@users.noreply.github.com>
2024-07-22 16:19:41 +0000

    Initial commit

---
+++ js/task-2.js
@@ -0,0 +1,18 @@
+"use strict";
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
+
+function makeArray(firstArray, secondArray, maxLength) {
+  const joinAll = firstArray.concat(secondArray);
+  if (joinAll.length <= maxLength) {
+    return joinAll;
+    
+  } else {
+    const slice = joinAll.slice(0, maxLength);
+    return slice
+  }
+}
